744469ce000cc892ba4b5ce28deec85405f787a8
lisw4y <lsw99u@gmail.com>
2019-02-02 00:25:05 -0800

    Add side drawer for mobile users

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+const sideDrawer = (props) => {
+  const attachedClasses = [
+    classes.SideDrawer,
+    (props.open ? classes.Open : classes.Close)
+  ];
+
+  return (
+    <>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default sideDrawer;
